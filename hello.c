8fe511b9528ec8c45464dfd3e71b418696664871
Jstuedebaker22 <111700061+Jstuedebaker22@users.noreply.github.com>
2022-08-30 02:03:41 +0000

    added my name and major

--- hello.c
+++ hello.c
@@ -4,6 +4,8 @@ int main(void)  {
 
     printf("Albina Kysorets!\n");
     printf("Computer Engineering\n");
+    printf("My name is Joe Stuedemann\n");
+    printf("My Major is Computer Science\n");
 
     return 0;
 }
